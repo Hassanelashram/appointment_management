b44e788f9bf0ff56e746fa1cd2efe340cca55e4b
hassan el ashram <elashramhassan@gmail.com>
2021-05-08 17:24:43 +0200

    more templating for appointments page while brainstorming

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :dentists, only: %i[index show] do
-    resources :appointments, only: %i[create index show]
+    resources :appointments, only: %i[create show]
   end
+  resources :appointments, only: :index
   root to: 'dentists#index'
 end
