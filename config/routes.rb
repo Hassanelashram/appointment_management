7b5aaeba0bedad9ac12b9161e6293679b30c5bc2
hassan el ashram <elashramhassan@gmail.com>
2021-05-08 16:50:43 +0200

    generated resource for dentist and for appointment and nested the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :dentists, only: %i[index show] do
+    resources :appointments, only: %i[create index show]
+  end
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_08_142057) do
+ActiveRecord::Schema.define(version: 2021_05_08_144710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "appointments", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "dentist_id", null: false
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["dentist_id"], name: "index_appointments_on_dentist_id"
+    t.index ["user_id"], name: "index_appointments_on_user_id"
+  end
+
+  create_table "dentists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +44,6 @@ ActiveRecord::Schema.define(version: 2021_05_08_142057) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "appointments", "dentists"
+  add_foreign_key "appointments", "users"
 end
