e9b2fea2e2e51896be14377eac273a65e5005638
hassan el ashram <elashramhassan@gmail.com>
2021-05-08 17:11:38 +0200

    added quick html to index and show for dentists

--- app/controllers/dentists_controller.rb
+++ app/controllers/dentists_controller.rb
@@ -1,7 +1,17 @@
 class DentistsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_dentist, only: [:show]
 
   def index
     @dentists = Dentist.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_dentist
+    @dentist = Dentist.find(params[:id])
   end
 end
